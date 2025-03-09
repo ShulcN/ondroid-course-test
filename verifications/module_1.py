2768c7e10c970b372ef8c02313c73ead3a896662
ShulcN <95365569+ShulcN@users.noreply.github.com>
2025-03-10 02:49:58 +0300

    Update module_1.py

--- verifications/module_1.py
+++ verifications/module_1.py
@@ -6,7 +6,7 @@ def text(frame, testData, robot):
     testData = {
       "end_time": time.time()+20
     }
-  text_for_output = "Module 1. Task text"
+  text_for_output = "Module 1. Task text robo"
   frame = robot.draw_info(frame)
   frame = cv2.putText(frame, text_for_output, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
   result = {
