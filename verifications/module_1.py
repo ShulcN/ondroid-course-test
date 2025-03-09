d12c368c4dec6aa4bd637af612c151eace81f1c0
ShulcN <95365569+ShulcN@users.noreply.github.com>
2025-03-10 02:05:46 +0300

    Create module_1.py

---
+++ verifications/module_1.py
@@ -0,0 +1,20 @@
+import cv2
+import time
+
+def text(frame, testData, robot):
+  if not testData:
+    testData = {
+      "end_time": time.time()+20
+    }
+  text_for_output = "Module 1. Task"
+  frame = robot.draw_info(frame)
+  frame = cv2.putText(frame, text_for_output, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+  text_for_output = f"Robot name: {robot.get_info['name']}"
+  frame = cv2.putText(frame, text_for_output, (50, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+  result = {
+    "success": True,
+    "description": "Perfect 2!",
+    "score":100
+  }
+  return frame, testData, text_for_output, result
+  
