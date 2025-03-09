32e9419428fdb7bae103ca21ca65293d8faf1297
ShulcN <95365569+ShulcN@users.noreply.github.com>
2025-03-10 00:51:19 +0300

    Update module_0.py

--- verifications/module_0.py
+++ verifications/module_0.py
@@ -9,8 +9,7 @@ def draw(frame, testData, robot):
     }
   testData["counter"] += 1
   text_for_output = f"Counter value: {testData['counter']}"
-  frame = cv2.putText(frame, text_for_output, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-    1, (0, 0, 255), 2, cv2.LINE_AA)
+  frame = robot.draw_info(frame);
   result = {
     "success": True,
     "description": "Perfect!",
